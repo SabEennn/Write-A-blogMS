ebea6cb45d4173c8a49a271915bb8dded4d7dd8f
SabEennn <sabinp7278@gmail.com>
2025-01-13 14:43:07 +0545

    improved /addblog route on UI aspect

--- src/components/navbar/layout/Layout.jsx
+++ src/components/navbar/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import Search from '../../../pages/blog/components/Search'
 
 const Layout = ({children}) => {
   return (
     <>
     <Navbar />
+    <Search />
     {children}
     </>
   )
