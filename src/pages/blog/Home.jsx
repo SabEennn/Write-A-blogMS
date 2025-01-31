9094b8d9a960841793d980bf2cad5e3d98806b37
SabEennn <sabinp7278@gmail.com>
2025-01-31 22:44:48 +0545

    working on get method

--- src/pages/blog/Home.jsx
+++ src/pages/blog/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../../components/layout/Layout";
 import Card from "./components/card/Card";
+import { baseurl } from "../../config";
 
 const Home = () => {
+  const [blogs, setBlogs] = useEffect([])
+  const fetchBlogs =async () =>{
+  const response =await axios.get(`${baseurl}/blogs`);
+  console.log(response);
+  }
+
+  useEffect( ()=>{
+    fetchBlogs();
+  }, [])
   return (
     <Layout>
       <div className="flex flex-wrap">
